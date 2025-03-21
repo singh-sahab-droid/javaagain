442ef98d32179a15f14dc59b42d72e59f945eafe
singh-sahab-droid <mukeshsinghjsrn95@gmail.com>
2025-03-21 23:47:23 +0530

    Create javanew.java

---
+++ javanew.java
@@ -0,0 +1,7 @@
+public class PrintNames {
+    public static void main(String[] args) {
+        System.out.println("Alice");
+        System.out.println("Bob");
+        System.out.println("Charlie");
+    }
+}
